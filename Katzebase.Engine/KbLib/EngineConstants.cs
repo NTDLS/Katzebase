49ac6495bca95ecb1e9d212d9f3d139dcd5963cc
Josh Patterson <josh@ntdls.com>
2023-06-15 12:08:46 -0400

    Page constants.

--- Katzebase.Engine/KbLib/EngineConstants.cs
+++ Katzebase.Engine/KbLib/EngineConstants.cs
@@ -3,12 +3,12 @@
     public static class EngineConstants
     {
         public const string DocumentPageExtension = ".kbpage";
-        public const string SchemaCatalogFile = "schemas.kbcat";
-        public const string DocumentPageCatalogFile = "pages.kbcat";
-        public const string IndexCatalogFile = "indexes.kbcat";
-        public const string LoginCatalogFile = "logins.kbcat";
-        public const string TransactionActionsFile = "transaction.kblog";
-        public const string HealthStatsFile = "health.json";
+        public const string SchemaCatalogFile = "@schemas.kbcat";
+        public const string DocumentPageCatalogFile = "@pages.kbcat";
+        public const string IndexCatalogFile = "@indexes.kbcat";
+        public const string LoginCatalogFile = "@logins.kbcat";
+        public const string TransactionActionsFile = "@transaction.kblog";
+        public const string HealthStatsFile = "@health.json";
         public static Guid RootSchemaGUID = Guid.Parse("0AABFAFA-5736-4BD9-BA74-E4998E137528");
 
         public enum IOFormat
