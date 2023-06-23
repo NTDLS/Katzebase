a5b3b076b30b087c56f66beb99a747e8a9a95b57
Josh Patterson <josh@ntdls.com>
2023-06-23 17:29:37 -0400

    Post rebuild cleanup,

--- Katzebase.Engine/Query/Searchers/DocumentLookupResult.cs
+++ Katzebase.Engine/Query/Searchers/DocumentLookupResult.cs
@@ -1,13 +1,8 @@
-﻿using Katzebase.Engine.Documents;
-
-namespace Katzebase.Engine.Query.Searchers
+﻿namespace Katzebase.Engine.Query.Searchers
 {
     public class DocumentLookupResult
     {
-        public Dictionary<string, DocumentPointer> SchemaDocumentPointers = new();
         public List<string> Values { get; set; } = new();
-
-        public Dictionary<string, string> ConditionFields = new();
 
         public DocumentLookupResult(List<string> values)
         {
