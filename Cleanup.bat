5c2c4f95fa8820e6d776e0edc4cfe3cab26ef236
Josh Patterson <josh@ntdls.com>
2023-08-04 15:29:04 -0400

    meh.

--- Cleanup.bat
+++ Cleanup.bat
@@ -2,17 +2,18 @@ attrib ".vs" -h -a -r -s
 attrib *.* -h -a -r -s
 
 rd ".vs" /S /Q
-rd "packages" /S /Q
+
+rd "rd "@Installers\Output" /S /Q
 
 rd "Katzebase.Engine\bin" /S /Q
-rd "Katzebase.Engine\obj" /S /Q
-rd "Katzebase.Library\bin" /S /Q
-rd "Katzebase.Library\obj" /S /Q
-rd "Katzebase.Models\bin" /S /Q
-rd "Katzebase.Models\obj" /S /Q
+rd "rd "Katzebase.Engine\obj" /S /Q
+rd "Katzebase.PrivateLibrary\bin" /S /Q
+rd "rd "Katzebase.PrivateLibrary\obj" /S /Q
+rd "Katzebase.PublicLibrary\bin" /S /Q
+rd "rd "Katzebase.PublicLibrary\obj" /S /Q
 rd "Katzebase.Service\bin" /S /Q
-rd "Katzebase.Service\obj" /S /Q
+rd "rd "Katzebase.Service\obj" /S /Q
 rd "Katzebase.TestHarness\bin" /S /Q
-rd "Katzebase.TestHarness\obj" /S /Q
-rd "Katzebase.CodeGeneration\bin" /S /Q
-rd "Katzebase.CodeGeneration\obj" /S /Q
+rd "rd "Katzebase.TestHarness\obj" /S /Q
+rd "Katzebase.UI\bin" /S /Q
+rd "rd "Katzebase.UI\obj" /S /Q
