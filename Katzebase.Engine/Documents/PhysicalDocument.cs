6191ce4e29dac97dde1c13018fe39fe908cfc9ee
Josh Patterson <josh@ntdls.com>
2023-08-08 16:40:00 -0400

    Experiment with delayed deflate materialization.

--- Katzebase.Engine/Documents/PhysicalDocument.cs
+++ Katzebase.Engine/Documents/PhysicalDocument.cs
@@ -1,4 +1,6 @@
-﻿using Katzebase.PublicLibrary;
+﻿using Katzebase.Engine.Library;
+using Katzebase.PublicLibrary;
+using Katzebase.PublicLibrary.Exceptions;
 using Katzebase.PublicLibrary.Types;
 using Newtonsoft.Json;
 using ProtoBuf;
@@ -12,8 +14,69 @@ namespace Katzebase.Engine.Documents
     [ProtoContract]
     public class PhysicalDocument
     {
+        [ProtoIgnore]
+        private KbInsensitiveDictionary<string?>? _dictionary = null;
+
+        [ProtoIgnore]
+        public KbInsensitiveDictionary<string?> Dictionary
+        {
+            get
+            {
+                if (_dictionary == null)
+                {
+                    if (CompressedBytes == null)
+                    {
+                        throw new KbNullException("Document compressed bytes cannot be null.");
+                    }
+
+                    var serializedData = Compression.Decompress(CompressedBytes);
+                    ContentLength = serializedData.Length;
+                    using (var input = new MemoryStream(serializedData))
+                    {
+                        _dictionary = Serializer.Deserialize<KbInsensitiveDictionary<string?>>(input);
+                        if (_dictionary == null)
+                        {
+                            throw new KbNullException("Document dictionary cannot be null.");
+                        }
+                        //TODO: Maybe theres a more optimistic way to do this. Other than RAM, there is no need to NULL out the other property
+                        //TODO:     This could lead to us de/serialize and de/compressing multiple times if we need to write a document.
+                        _compressedBytes = null; //For memory purposes, we want to store either compressed OR uncompressed - but not both.
+                    }
+                }
+                return _dictionary;
+            }
+        }
+
+        public byte[]? _compressedBytes = null;
+
         [ProtoMember(1)]
-        public KbInsensitiveDictionary<string?> Dictionary = new();
+        public byte[]? CompressedBytes
+        {
+            get
+            {
+                if (_compressedBytes == null)
+                {
+                    using (var output = new MemoryStream())
+                    {
+                        Serializer.Serialize(output, _dictionary);
+                        ContentLength = (int)output.Length;
+                        _compressedBytes = Compression.Compress(output.ToArray());
+                        //TODO: Maybe theres a more optimistic way to do this. Other than RAM, there is no need to NULL out the other property
+                        //TODO:     This could lead to us de/serialize and de/compressing multiple times if we need to write a document.
+                        _dictionary = null; //For memory purposes, we want to store either compressed OR uncompressed - but not both.
+                    }
+                }
+
+                return _compressedBytes;
+            }
+            set
+            {
+                _compressedBytes = value;
+                //TODO: Maybe theres a more optimistic way to do this. Other than RAM, there is no need to NULL out the other property
+                //TODO:     This could lead to us de/serialize and de/compressing multiple times if we need to write a document.
+                _dictionary = null; //For memory purposes, we want to store either compressed OR uncompressed - but not both.
+            }
+        }
 
         [ProtoMember(2)]
         public DateTime Created { get; set; }
@@ -21,8 +84,8 @@ namespace Katzebase.Engine.Documents
         [ProtoMember(3)]
         public DateTime Modfied { get; set; }
 
-        [ProtoIgnore]
-        public int ContentLength => Dictionary.Sum(o => o.Key.Length + (o.Value?.Length ?? 0));
+        [ProtoMember(4)]
+        public int ContentLength { get; set; }
 
         public PhysicalDocument()
         {
@@ -30,16 +93,22 @@ namespace Katzebase.Engine.Documents
 
         public PhysicalDocument(string jsonString)
         {
+            SetDictionaryByJson(jsonString);
+        }
+
+        public void SetDictionaryByJson(string jsonString)
+        {
             var dictionary = JsonConvert.DeserializeObject<KbInsensitiveDictionary<string?>>(jsonString);
             KbUtility.EnsureNotNull(dictionary);
-            Dictionary = dictionary;
+            _dictionary = dictionary;
         }
 
         public PhysicalDocument Clone()
         {
             return new PhysicalDocument
             {
-                Dictionary = Dictionary,
+                _compressedBytes = _compressedBytes,
+                _dictionary = _dictionary,
                 Created = Created,
                 Modfied = Modfied
             };
