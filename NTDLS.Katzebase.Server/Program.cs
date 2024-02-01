46435807ba141e59f128f0203d4f3f8ad1d911c4
Josh Patterson <josh@ntdls.com>
2024-02-01 09:33:32 -0500

    Removed non-server code.

---
+++ NTDLS.Katzebase.Server/Program.cs
@@ -0,0 +1,73 @@
+using NTDLS.Katzebase.Server;
+using Topshelf;
+
+namespace NTDLS.Katzebase.Server
+{
+    public class Program
+    {
+        public class KatzebaseService
+        {
+            private SemaphoreSlim _semaphoreToRequestStop;
+            private Thread _thread;
+
+            public KatzebaseService()
+            {
+                _semaphoreToRequestStop = new SemaphoreSlim(0);
+                _thread = new Thread(DoWork);
+            }
+
+            public void Start()
+            {
+                _thread.Start();
+            }
+
+            public void Stop()
+            {
+                _semaphoreToRequestStop.Release();
+                _thread.Join();
+            }
+
+            private void DoWork()
+            {
+                var apiService = new APIService();
+
+                apiService.Start();
+
+                while (true)
+                {
+                    if (_semaphoreToRequestStop.Wait(500))
+                    {
+                        apiService.Stop();
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.StartAutomatically(); // Start the service automatically
+
+                x.EnableServiceRecovery(rc =>
+                {
+                    rc.RestartService(1); // restart the service after 1 minute
+                });
+
+                x.Service<KatzebaseService>(s =>
+                {
+                    s.ConstructUsing(hostSettings => new KatzebaseService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Katzebase document-based database services.");
+                x.SetDisplayName("Katzebase Service");
+                x.SetServiceName("Katzebase");
+            });
+        }
+    }
+}
--- NTDLS.Katzebase.UI/UISettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NTDLS.Katzebase.UI
-{
-    internal class UISettings
-    {
-        public int MaxResultRows { get; set; } = 10000;
-    }
-}
