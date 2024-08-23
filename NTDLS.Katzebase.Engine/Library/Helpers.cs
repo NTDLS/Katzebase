783dccd0862bb0c10dd441b023db8cd5f6fb807c
Josh Patterson <Josh@NTDLS.com>
2024-08-23 12:57:29 -0400

    wip.

--- NTDLS.Katzebase.Engine/Library/Helpers.cs
+++ NTDLS.Katzebase.Engine/Library/Helpers.cs
@@ -22,6 +22,7 @@ namespace NTDLS.Katzebase.Engine.Library
 
         /// <summary>
         /// Produces a new dictionary that is the product of the common keys between the two.
+        /// If the given dictionary is null, a clone of dictionary two is returned.
         /// </summary>
         public static Dictionary<K, V> Intersect<K, V>(this Dictionary<K, V>? one, Dictionary<K, V> two) where K : notnull
         {
