c110eeefc04dd5476b61350c506747c87b416f30
ingted <anibal.yeh@gmail.com>
2024-09-25 16:54:45 +0800

    where_condition_parsing_test_case

--- TestApplications/NTDLS.Katzebase.UnitTests/Program.ver.0.0.1.fsx
+++ TestApplications/NTDLS.Katzebase.UnitTests/Program.ver.0.0.1.fsx
@@ -5,5 +5,6 @@ open BasicTest.Parser.ver.``0``.``0``.``1``
 open BasicTest.IO.ver.``0``.``0``.``1``.IOTests
 
 module KatzebaseTests =
-    ParserBasicTests.``Parse "SELECT * FROM MASTER:ACCOUNT"`` None    
+    ParserBasicTests.``Parse "SELECT * FROM MASTER:ACCOUNT"`` None 
+    ParserBasicTests.``[Condition] Parse "SELECT * FROM MASTER:ACCOUNT WHERE Username = ?Username AND PasswordHash = ?PasswordHash"`` None
     printfn "Done!"        
