d75ea0306d499399ffce3b984c3a532f655810d8
Josh Patterson <Josh@NTDLS.com>
2024-10-03 12:56:50 -0400

    Library update.

--- TestApplications/QueryTest/Program.cs
+++ TestApplications/QueryTest/Program.cs
@@ -17,30 +17,6 @@ namespace QueryTest
             {
                 //using var client = new KbClient(_serverHost, _serverPort, _username, KbClient.HashPassword(_password));
 
-                var dtp = new DelegateThreadPool();
-
-                var childQueue = dtp.CreateChildQueue<Guid>();
-
-
-                for (int item = 0; item < 10000; item++)
-                {
-                    childQueue.Enqueue(Guid.NewGuid(), (param) =>
-                    {
-                        for (int workload = 0; workload < 10000; workload++)
-                        {
-                            foreach (var c in param.ToString())
-                            {
-                            }
-                        }
-                    });
-                }
-
-                childQueue.WaitForCompletion();
-
-                foreach (var thread in dtp.Threads)
-                {
-                    Console.WriteLine($"Thread: {thread.ManagedThread.ManagedThreadId}: {thread.NativeThread?.TotalProcessorTime.TotalMilliseconds:n0} ");
-                }
             }
             catch (Exception ex)
             {
